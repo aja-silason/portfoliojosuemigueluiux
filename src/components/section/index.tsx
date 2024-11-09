e1d813430c2f5e4dc4568287370a8ffb02c32116
Anania Augusto <anania.augusto@baxtter.local>
2024-11-09 12:26:25 +0100

    style(section): change style in file

--- src/components/section/index.tsx
+++ src/components/section/index.tsx
@@ -10,7 +10,7 @@ export const StartNowSection = () => {
             <span className="z-10 text-center flex flex-col gap-[.5em] items-center">
                 <p className="text-[#FFFFFFB2] font-[grotesk] font-[700] text-[16px]">COMECE AGORA</p>
                 <h3 className="text-[#FFFFFF] font-[grotesk] font-[700] text-[40px]">PRONTO PARA TRANSFORMAR O SEU NEGÓCIO?</h3>
-                <Link href="/about" className="bg-[#FFFFFF] p-[14px] rounded-[32px]">
+                <Link href="/about" className="bg-[#FFFFFF] hover:bg-[#FFFFF2] transition p-[14px] rounded-[32px]">
                     <span className="text-[#0E0C15] font-[grotesk] font-[900] text-[16px]">Inicie seu Projecto</span>
                 </Link>
             </span>
